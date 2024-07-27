9379b965ae8f7ae476a47ea9e9a15bfbd528eb60
Strelitzia <1492846643@qq.com>
2024-07-27 11:35:50 +0800

    完成限流

--- src/main/java/com/yupi/springbootinit/manager/RedissonLimiterManager.java
+++ src/main/java/com/yupi/springbootinit/manager/RedissonLimiterManager.java
@@ -2,19 +2,25 @@ package com.yupi.springbootinit.manager;
 
 import com.yupi.springbootinit.common.ErrorCode;
 import com.yupi.springbootinit.exception.BusinessException;
+import lombok.Data;
 import org.redisson.api.RRateLimiter;
 import org.redisson.api.RateIntervalUnit;
 import org.redisson.api.RateType;
 import org.redisson.api.RedissonClient;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 
 @Component
+@ConfigurationProperties(prefix = "spring.redisson.limiter")
+@Data
 public class RedissonLimiterManager {
 
     @Resource
     private RedissonClient redissonClient;
+
+    private Integer maxRate;
 
     /**
      * 限流操作
@@ -23,7 +29,7 @@ public class RedissonLimiterManager {
     public void doRateLimit(String key) {
         RRateLimiter rateLimiter = redissonClient.getRateLimiter(key);
         // 每个时间单位允许5次请求，每个时间单位有1s
-        rateLimiter.trySetRate(RateType.OVERALL, 5, 1, RateIntervalUnit.SECONDS);
+        rateLimiter.trySetRate(RateType.OVERALL, maxRate, 1, RateIntervalUnit.SECONDS);
 
         // 每当一个请求来了后，获取1个令牌
         boolean canOp = rateLimiter.tryAcquire(1);
